fc484d748e01db1e1443111323706de2462d7f02
Tandian <126342653+Binta02@users.noreply.github.com>
2025-04-29 13:21:53 +0200

    import

--- src/pages/ServiceDetail.tsx
+++ src/pages/ServiceDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState  } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { Card, CardContent } from '@/components/ui/card';
