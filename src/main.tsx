e0ccc8dd3c3664ee9c72d4c81cc11ec66f6e0d41
Tandian <126342653+Binta02@users.noreply.github.com>
2025-05-18 22:21:29 +0200

    resolution des erreurs

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.tsx";
-import "./index.css";
-import "react-datepicker/dist/react-datepicker.css";
-import { CartProvider } from "./components/cart/CartContext";
 
-const queryClient = new QueryClient();
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
+import { CartProvider } from './components/cart/CartContext'
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </QueryClientProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
   </BrowserRouter>
 );
