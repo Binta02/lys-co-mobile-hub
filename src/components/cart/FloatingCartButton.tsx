fd53df60a641262fdb5925ff709bb6bd99111cfe
Tandian <126342653+Binta02@users.noreply.github.com>
2025-05-27 08:43:18 +0000

    style du panier

--- src/components/cart/FloatingCartButton.tsx
+++ src/components/cart/FloatingCartButton.tsx
@@ -12,19 +12,17 @@ export function FloatingCartButton() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <div className="fixed bottom-4 right-4 z-50">
-          <Button variant="outline" size="icon" className="relative">
-            <ShoppingCart className="h-5 w-5" />
-            {items.length > 0 && (
-              <Badge
-                variant="destructive"
-                className="absolute -top-1 -right-1 h-5 w-5 flex items-center justify-center p-0 text-xs"
-              >
-                {items.length}
-              </Badge>
-            )}
-          </Button>
-        </div>
+        <Button variant="outline" size="icon" className="relative">
+          <ShoppingCart className="h-4 w-4" />
+          {items.length > 0 && (
+            <Badge 
+              className="absolute -top-2 -right-2 h-5 w-5 flex items-center justify-center p-0 text-xs"
+              variant="destructive"
+            >
+              {items.length}
+            </Badge>
+          )}
+        </Button>
       </SheetTrigger>
       <SheetContent side="right" className="w-[400px] sm:w-[540px] flex flex-col">
         <CartDrawer />
